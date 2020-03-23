fd049d78694ad9020f8f5808939e7ceae95622f7
Desperate Hiking <perevechenkov1999@gmail.com>
2020-03-23 18:28:57 +0500

    dependencies, build and procfile

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const staticPath = path.join(__dirname, '/build');
+app.use(express.static(staticPath));
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <h1>Team-6 </h1>
+        <li>Степанов В.А. (капитан) - <a href="https://github.com/VladimirSTAR0911">Git</a></li>
+        <li>Артемьева М.С. - <a href="https://github.com/mashaartemeva">Git</a></li>
+        <li>Перевеченков В.В. - <a href="https://github.com/DesperateHiking">Git</a></li>
+        <li>Уруджев Р.Н. - <a href="https://github.com/zoomfix">Git</a></li>
+        <li>Костин С.А. - <a href="https://github.com/semen-kostin">Git</a></li>
+      </div>
+  );
+}
+
+export default App;
